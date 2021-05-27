b570ce62831e9a712b8c2f91f4275106122682ef
avarlamova <59831804+avarlamova@users.noreply.github.com>
2021-05-27 11:34:27 +0300

    further practice

---
+++ lower case.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ * Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+ */
+ var toLowerCase = function(s) {
+    let res='';
+    s.split('').map(el=> {
+        let code = el.charCodeAt(0)
+        if (code<91&&code>64) {
+        res+=String.fromCharCode(code+32)
+        }
+        else res+=el
+    })
+    return res
+};
