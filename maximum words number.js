3a5b9465cbf1cadb0cb56bb5672fc5e5e4dc967e
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-01-10 23:40:33 +0300

    further practice

---
+++ maximum words number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ * A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+Return the maximum number of words that appear in a single sentence
+ */
+var mostWordsFound = function (sentences) {
+  let max = sentences[0].split(" ").length;
+
+  for (let i = 1; i < sentences.length; i++) {
+    if (sentences[i].split(" ").length > max)
+      max = sentences[i].split(" ").length;
+  }
+  return max;
+};
