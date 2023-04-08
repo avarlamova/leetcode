163faac2a95dcd2e90bf86174ac768d613195a46
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-04-08 17:49:03 +0700

    Create deeplyNesteObj.js

---
+++ deeplyNesteObj.js
@@ -0,0 +1,21 @@
+// дан объект и путь его свойства. Вернуть или то, что лежит в свойстве, или undefined
+function get(obj, path) {
+  const objKeys = path.split(".");
+  if (objKeys.length > 1) {
+    const currentKey = objKeys.shift();
+    return get(obj[currentKey], objKeys.join("."));
+  } else {
+    const currentKey = objKeys[0];
+    return obj[currentKey];
+  }
+}
+
+let obj = {
+  a: "a",
+  b: {
+    c: "f",
+    d: "t",
+  },
+};
+console.log("answer: ", get(obj, "b.c"));
+// get(obj, "b.c");
